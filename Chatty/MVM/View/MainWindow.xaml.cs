59952e938fac8a74bbee3eaa2bf1dabf3a07657a
Ptichka222 <62928966+NeDoWaS@users.noreply.github.com>
2026-03-14 23:28:39 +0500

    finally gonna work on sme ui

--- Chatty/MVM/View/MainWindow.xaml.cs
+++ Chatty/MVM/View/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Shapes;
 namespace Chatty
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
